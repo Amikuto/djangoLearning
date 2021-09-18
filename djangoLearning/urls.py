4074e51c7e709bfbacf29d05174a17316f4a1109
Damir Kurtaev <56434766+Amikuto@users.noreply.github.com>
2021-09-18 17:37:56 +0300

    Favicon and bg
    Добавлена фавиконка и задник (хэдэра или пока не решил чего). Так же прописана фавиконка в юрлах

--- djangoLearning/urls.py
+++ djangoLearning/urls.py
@@ -3,11 +3,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
+from django.views.generic.base import RedirectView
 
 from main import views
+
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('', views.index),
     path('about/', views.about),
     path('admin/', admin.site.urls),
+    re_path(r'^favicon\.ico$', favicon_view),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
